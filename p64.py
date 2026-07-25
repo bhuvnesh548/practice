b54b21d03429714b0b0d9df2352dd78b8b42f7db
bhuvnesh <bhuvneshpandey23@gmail.com>
2026-07-25 19:10:24 +0530

    Initial Commit

---
+++ p64.py
@@ -0,0 +1,5 @@
+# Write a program that takes three name (or any words) from a user in a single input prompt and assigns them to three separate variables.
+str1, str2, str3 = input("Enter name1: "),input("Enter name2: "),input("Enter name3: ")
+print(f"name1={str1}")
+print(f"name2={str2}")
+print(f"name3={str3}")
