8a2c59ca650121af402e8105b0c36bdfe26b71d5
bhuvnesh <bhuvneshpandey23@gmail.com>
2026-08-11 16:30:13 +0530

    Initial Commit

---
+++ program150.py
@@ -0,0 +1,4 @@
+# Calculate the cube of all numbers from 1 to a given number
+n=int(input("enter a number :"))
+for i in range(1,n+1):
+    print(f"the current number is {i} and the cube is {i*i*i}")
