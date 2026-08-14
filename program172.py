87a9d0f93381f31cad9c9c79d7a59bd407683869
bhuvnesh <bhuvneshpandey23@gmail.com>
2026-08-14 12:38:43 +0530

    Initial Commit

---
+++ program172.py
@@ -0,0 +1,9 @@
+# Write a script to perform the following three operations on given list
+
+# Access the third element of a list
+# List Length: Print the total number of items
+# # Check if the list is empty
+list = [10, 20, 30, 40, 50]
+print(list[3-1])
+print(len(list))
+print(len(list)<0)
