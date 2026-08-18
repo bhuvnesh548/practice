0c09ecbdacbe36d88caab137524f77f5bf4c9fe3
bhuvnesh <bhuvneshpandey23@gmail.com>
2026-08-18 22:24:10 +0530

    Update program183.py

--- program183.py
+++ program183.py
@@ -1,7 +1,6 @@
 import pandas as pd
 series=pd.Series([1,2,3,4,5],index=["a","b","c","d","e"])
 print (series)
-series2=pd.DataFrame({"name":["bhuvnesh","abhay","priya"], "marks":[30,20,10]})
-print(series2)
-series3=pd.read_csv("data.csv")
-print(series3)
+df=pd.read_csv("data.csv")
+df.info()
+print(df)
